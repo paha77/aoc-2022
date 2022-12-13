606da6251ec1a277067425bf62400c6304de668c
Péter Bakonyi-Kárpáti <pbk@vivid-planet.com>
2022-12-13 16:49:50 +0100

    Day 13 Task 2

--- day13task2.php
+++ day13task2.php
@@ -5,15 +5,26 @@ $input = file_get_contents(__DIR__ . '/input.txt');
 $sum = 0;
 foreach (explode("\n\n", $input) as $index => $pair) {
     $pair = explode("\n", $pair);
-    echo sprintf("Comparing (Index: %d) %s <=> %s\n", $index+1, $pair[0], $pair[1]);
-    $pair = array_map(fn ($item) => json_decode($item), $pair);
-
-    if (compare($pair[0], $pair[1])) {
-        echo "\tOK\n";
-        $sum += $index +1;
+    $pairs = [];
+    foreach (explode("\n\n", $input) as $index => $pair) {
+        $pairs[] = array_map(fn ($item) => json_decode($item), explode("\n", $pair));
     }
+    $pairs[] = [[[2]], [[6]]];
 
-    var_dump($sum);
+    $prod = 1;
+    foreach ($pairs as $index => $pair) {
+
+        if ($pair[0] === [[2]] || $pair[1] === [[2]] || $pair[0] === [[6]] || $pair[1] === [[6]]) {
+            continue;
+        }
+
+        echo sprintf("Comparing (Index: %d) %s <=> %s\n", $index+1, $this->flatten($pair[0]), $this->flatten($pair[1]));
+        if ($this->compare($pair[0], $pair[1])) {
+            echo "\tOK\n";
+            $prod *= $index + 1;
+        }
+    }
+    var_dump($prod);
 }
 function compare(array|int $left, array|int $right): bool
 {
