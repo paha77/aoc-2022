1a698a54872e93f3346bb27871d1575cdaa0baa8
Péter Bakonyi-Kárpáti <pbk@vivid-planet.com>
2022-12-14 17:04:38 +0100

    Day 14 Task 1

--- day14task1.php
+++ day14task1.php
@@ -1,6 +1,7 @@
 <?php
 
 $input = file_get_contents(__DIR__ . '/test.txt');
+//$input = file_get_contents(__DIR__ . '/input1.txt');
 $cave = array_reduce(explode("\n", $input), function ($cave, $lineToParse) {
     /** @var Cave $cave */
     if (preg_match_all('/([0-9]+,[0-9]+)?/', $lineToParse, $matches)) {
@@ -9,7 +10,7 @@ $cave = array_reduce(explode("\n", $input), function ($cave, $lineToParse) {
             if (mb_strlen($possiblePoint) == 0) {
                 return $points;
             }
-            return array_merge($points, [new Point(explode(',', $possiblePoint), null, '#')]);
+            return array_merge($points, [explode(',', $possiblePoint)]);
         }, []) as $point) {
             if (is_null($startPoint)) {
                 $startPoint = $point;
@@ -24,84 +25,176 @@ $cave = array_reduce(explode("\n", $input), function ($cave, $lineToParse) {
     return $cave;
 }, new Cave());
 
+//$m = memory_get_usage();
+echo $cave->pour() . "\n\n";
+//$m2 = memory_get_usage();
+//var_dump($m2 - $m);
 echo $cave;
+
 
 class Cave
 {
     public $points = [];
-    public $lowestX = PHP_INT_MAX;
-    public $highestX = 0;
-    //public $lowestY = PHP_INT_MAX;
-    public $lowestY = 0;
-    public $highestY = 0;
+    public $sandPouringPoint;
 
-    public function drawLine(Point $start, Point $end)
+    public function __construct()
     {
-        //echo sprintf("Drawing line with start: %s and end: %s\n", $start, $end);
+        $this->sandPouringPoint = [500, 0];
+    }
 
+    public function pour()
+    {
+        $sandsPoured = 0;
+        do {
+            $this->points[$this->sandPouringPoint[0]][$this->sandPouringPoint[1]] = 'o';
+            if (!$this->fall($this->sandPouringPoint)) {
+                break;
+            }
+            $sandsPoured++;
+        } while (true);
+
+        return $sandsPoured;
+    }
+
+    public function fall(array $point): bool
+    {
+        $newX = $point[0];
+        $newY = $point[1];
+        if ($this->__canFallDown($point)) {
+            $newY++;
+        } elseif ($this->__canFallDownAndLeft($point)) {
+            $newX--;
+            $newY++;
+        }  elseif ($this->__canFallDownAndRight($point)) {
+            $newX++;
+            $newY++;
+        }
+
+        // in rest
+        if ($newX === $point[0] && $newY === $point[1]) {
+            return true;
+        }
+
+        // start to fall one step
+        unset($this->points[$point[0]][$point[1]]);
+
+        // point is over the edge
+        if ($newY > $this->highestY() || $newX < $this->lowestX() || $newX > $this->highestX() ) {
+            return false;
+        }
+
+        $this->points[$newX][$newY] = 'o';
+
+        // keep falling
+        // echo sprintf("Fall: %d, %d\n", $newX, $newY);
+        return $this->fall([$newX, $newY]);
+    }
+
+    private function __canFallDown(array $point): bool
+    {
+        return !isset($this->points[$point[0]][$point[1] + 1]);
+    }
+
+    private function __canFallDownAndLeft(array $point): bool
+    {
+        return !isset($this->points[$point[0] - 1][$point[1] + 1]);
+    }
+
+    private function __canFallDownAndRight(array $point): bool
+    {
+        return !isset($this->points[$point[0] + 1][$point[1] + 1]);
+    }
+
+    public function drawLine($start, $end)
+    {
         // horizontal line
-        if ($start->y === $end->y) {
-            for ($x = min($start->x, $end->x); $x <= max($start->x, $end->x); $x++) {
-                $this->points[$x][$start->y] = new Point($x, $start->y, $start->type);
+        if ($start[1] === $end[1]) {
+            for ($x = min($start[0], $end[0]); $x <= max($start[0], $end[0]); $x++) {
+                $this->points[$x][$start[1]] = '#';
             }
         }
         // vertical line
-        if ($start->x === $end->x) {
-            for ($y = min($start->y, $end->y); $y <= max($start->y, $end->y); $y++) {
-                $this->points[$start->x][$y] = new Point($start->x, $y, $start->type);
+        if ($start[0] === $end[0]) {
+            for ($y = min($start[1], $end[1]); $y <= max($start[1], $end[1]); $y++) {
+                $this->points[$start[0]][$y] = '#';
             }
         }
-        $this->recalculate();
-        echo $this;
+        $this->__rearrange();
     }
 
     public function __toString()
     {
-        $ret = '';
+        $lowestY = $this->lowestY();
+        $ret = sprintf(
+            "[Dimensions: %d x %d | Sand: %d | Lowest X: %d Highest X: %d Lowest Y: %d Highest Y: %d]\n",
+            ($highestX = $this->highestX()) - ($lowestX = $this->lowestX()) + 1,
+            ($highestY = $this->highestY()) + 1,
+            $this->countSand(),
+            $lowestX,
+            $highestX,
+            $lowestY,
+            $highestY
+        );
 
-        for ($y = $this->lowestY; $y <= $this->highestY; $y++) {
+        for ($y = $lowestY; $y <= $highestY; $y++) {
+
             $row = '';
-            for ($x = $this->lowestX; $x <= $this->highestX; $x++) {
-                $row .= isset($this->points[$x][$y]) ? $this->points[$x][$y]->type : '.';
+
+            if ($y === $lowestY) {
+                $row .= ' ';
+                for ($i = 0; $i <= $highestX - $lowestX; $i++) {
+                    $row .= $i%10;
+                }
+                $row .= "\n";
+            }
+
+            $row .= $y%10;
+
+            for ($x = $lowestX; $x <= $highestX; $x++) {
+                $row .= $this->points[$x][$y] ?? '.';
             }
             $ret .= $row . "\n";
         }
 
-        return $ret;
+        return $ret . "\n";
     }
 
-    private function recalculate()
+    public function countSand()
     {
-        $this->lowestX = min(array_keys($this->points));
-        $this->highestX = max(array_keys($this->points));
-        foreach ($this->points as $x => $columns) {
-            //$this->lowestY = min(array_merge(array_keys($columns), [$this->lowestY]));
-            $this->highestY = max(array_merge(array_keys($columns), [$this->lowestY]));
+        return array_reduce($this->points, function ($count, $rows) {
+            return $count + array_reduce($rows, function ($count, $point) {
+                return $count + ($point === 'o' ? 1 : 0);
+            }, 0);
+        });
+    }
+
+    private function __rearrange()
+    {
+        ksort($this->points);
+        foreach ($this->points as $x => $rows) {
+            ksort($this->points[$x]);
         }
-        //var_dump($this->lowestX, $this->highestX, $this->lowestY, $this->highestY);
-    }
-}
-
-class Point
-{
-    public $x;
-    public $y;
-    public $type = '#';
-
-    public function __construct($x, $y=null, $type = '#')
-    {
-        if (is_array($x)) {
-            $this->x = $x[0];
-            $this->y = $x[1];
-        } else {
-            $this->x = $x;
-            $this->y = $y;
-        }
-        $this->type = $type;
     }
 
-    public function __toString()
+    private function lowestX()
     {
-        return sprintf("[%d,%d]", $this->x, $this->y);
+        return min(array_keys($this->points));
+    }
+
+    private function highestX()
+    {
+        return max(array_keys($this->points));
+    }
+
+    private function lowestY(): int
+    {
+        return 0;
+    }
+
+    private function highestY(): int
+    {
+        return array_reduce($this->points, function ($highestY, $rows) {
+            return max($highestY, max(array_keys($rows)));
+        }, 0);
     }
 }
